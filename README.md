290a713ab3b59fbda2d6d438667192b3c5fd56a5
Jason Bacon <bacon@coral.acadix.biz>
2021-01-26 19:36:02 -0600

    Import source code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # runas
-Simple wrapper around su to make executing single commands cleaner
+Simple wrapper around su to make executing single commands cleaner.
+
+The su command is cumbersome to use for running single commands, requiring
+the -c and -m flags as well as quotes around the command, e.g.
+
+    su -m root -c 'command with arguments'
+
+The runas command, in contrast, minimizes requirements on the user:
+
+    runas root command with arguments
+
+Runas does not require any special privileges or configuration.  Users
+must enter the password of the target user (not their own password as with
+sudo) in order to execute a command.
---
+++ runas.c
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *  Description:
+ *      Impersonate another user
+ *      Simple wrapper around su, which is cumbersome for running
+ *      single commands, requiring quotes around the command, e.g.
+ *
+ *      su -m root -c 'command with arguments'
+ *
+ *  History: 
+ *  Date        Name        Modification
+ *  2021-01-26  J Bacon     Begin
+ ***************************************************************************/
+
+#include <stdio.h>
+#include <sysexits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define CMD_MAX     1024
+
+void    usage(char *argv[])
+
+{
+    fprintf(stderr, "Usage: %s username command\n", argv[0]);
+    exit(EX_USAGE);
+}
+
+int     main(int argc,char *argv[])
+
+{
+    char    cmd[CMD_MAX + 1] = "",
+	    *user;
+    int     c;
+    
+    if ( argc < 3 )
+	usage(argv);
+
+    user = argv[1];
+    for (c = 2; c < argc; ++c)
+    {
+	strlcat(cmd, argv[c], CMD_MAX);
+	strlcat(cmd, " ", CMD_MAX);
+    }
+    argv[0] = "su";
+    execlp("su", "-m", user, "-c", cmd, NULL);
+    return EX_OK;
+}
