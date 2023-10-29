b6563d210a0518dbf1adf0de6436bc2cf6088fcc
Jason W. Bacon <bacon4000@gmail.com>
2023-10-29 06:22:00 -0500

    Allow user to control "su" flags

--- runas.c
+++ runas.c
@@ -18,6 +18,8 @@
 #include <unistd.h>
 #include <limits.h>
 
+#define ARGV_MAX 1024
+
 #ifdef __linux__
 #define ARG_MAX 2097152 // getconf ARG_MAX CentOS 7
 #endif
@@ -27,9 +29,9 @@ void    usage(char *argv[])
 
 {
     if ( strstr(argv[0], "asroot") != NULL )
-	fprintf(stderr, "Usage: %s command\n", argv[0]);
+	fprintf(stderr, "Usage: %s [su flags] command\n", argv[0]);
     else
-	fprintf(stderr, "Usage: %s username command\n", argv[0]);
+	fprintf(stderr, "Usage: %s username [su flags] command\n", argv[0]);
     exit(EX_USAGE);
 }
 
@@ -39,6 +41,10 @@ void    usage(char *argv[])
 #define LOGIN_ENV   "-l"
 #endif
 
+// Invoke using absolute pathname to thwart Trojan su
+#define SU          "/usr/bin/su"
+
+// Could use libxtend, but avoiding a dependency just for one function
 #ifdef __linux__
 size_t  strlcat(char *dest,const char *src,size_t maxlen)
 
@@ -59,14 +65,28 @@ size_t  strlcat(char *dest,const char *src,size_t maxlen)
 }
 #endif
 
+void    check_argv_max(char *cmd, int argc)
+
+{
+    if ( argc >= ARGV_MAX )
+    {
+	fprintf(stderr, "%s: Too many command arguments.  Max = %d.\n", cmd, ARGV_MAX);
+	fprintf(stderr, "Increase ARGV_MAX and recompile if this is really necessary.\n");
+	exit(EX_DATAERR);
+    }
+}
+
+
 int     main(int argc,char *argv[])
 
 {
-    char    cmd[ARG_MAX + 1] = "",
-	    *user;
+    char    *exec_argv[ARGV_MAX],
+	    *user,
+	    cmd[ARG_MAX];
     int     c,
 	    min_args,
-	    first_arg;
+	    first_arg,
+	    exec_argc;
 
     if ( strstr(argv[0], "asroot") != NULL )
     {
@@ -84,21 +104,55 @@ int     main(int argc,char *argv[])
     if ( argc < min_args )
 	usage(argv);
 
+    exec_argv[0] = SU;
+    
     // FIXME: Warn if executable is not owned by root or is writable
     // by non-root users
     
-    for (c = first_arg; c < argc; ++c)
+    // Add su flags
+    for (c = first_arg, exec_argc = 1; *argv[c] == '-'; ++c, ++exec_argc)
     {
-	strlcat(cmd, argv[c], ARG_MAX);
-	strlcat(cmd, " ", ARG_MAX);
+	check_argv_max(argv[0], exec_argc);
+	exec_argv[exec_argc] = argv[c];
     }
+    
+    // Username and -c to precede command
+    check_argv_max(argv[0], exec_argc);
+    exec_argv[exec_argc++] = user;
+    
+    check_argv_max(argv[0], exec_argc);
+    exec_argv[exec_argc++] = "-c";
+    
+    // Command must be one argument to execvp()
+    *cmd = '\0';
+    while ( c < argc )
+    {
+	strlcat(cmd, argv[c++], ARG_MAX);
+	if ( c < argc )
+	    strlcat(cmd, " ", ARG_MAX);
+    }
+    
+    if ( strlen(cmd) == ARG_MAX )
+    {
+	fprintf(stderr, "%s: Command too long.\n", argv[0]);
+	exit(EX_DATAERR);
+    }
+    
+    check_argv_max(argv[0], exec_argc);
+    exec_argv[exec_argc++] = cmd;
+    
+    // Terminate argv array
+    exec_argv[exec_argc] = NULL;
+    
+    //for (c = 0; exec_argv[c] != NULL; ++c)
+    //    puts(exec_argv[c]);
+    
     /* Indicate which user's password must be entered */
     if ( geteuid() != 0 )
     {
 	printf("%s ", user);
 	fflush(stdout);
     }
-    argv[0] = "su";
-    execlp("su", "su", LOGIN_ENV, user, "-c", cmd, NULL);
+    execvp(SU, exec_argv);
     return EX_OK;
 }
